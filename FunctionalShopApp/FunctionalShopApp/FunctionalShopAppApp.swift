13187dbf42ee5c3720b61c36e9fa644ad193e0a3
Temur Chitashvili <temur.chitashvilii@gmail.com>
2025-04-11 21:12:20 +0400

    Inital Commit

---
+++ FunctionalShopApp/FunctionalShopApp/FunctionalShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionalShopAppApp.swift
+//  FunctionalShopApp
+//
+//  Created by Temur Chitashvili on 11.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FunctionalShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
